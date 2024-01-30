683a4aaaec0522b6f7b0eb297f80952545410543
elenlac <e.lacalle.2023@alumnos.urjc.es>
2024-01-30 15:55:13 +0100

    My first but not last comment

---
+++ S02/sum.py
@@ -0,0 +1,17 @@
+#Add the 20 first integer numbers
+res = 0
+
+for i in range(1, 21):
+    res += i
+
+print("Total sum: ", res)
+
+
+#Now with a list
+
+l = []
+
+for i in range(1, 21):
+    l.append(i)
+
+print("List: ", l)
