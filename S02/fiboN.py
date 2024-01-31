5e76b6bc93446339ff7fb7c09b14b6a870f963a2
elenlac <e.lacalle.2023@alumnos.urjc.es>
2024-01-31 17:31:53 +0100

    Change fibo

--- S02/fiboN.py
+++ S02/fiboN.py
@@ -1,13 +1,13 @@
 def fibon(n):
     first_number = 0
     second_number = 1
-    for n in range(0, n+1):
-        print(first_number, end=" ")
-        third_number = first_number + second_number
-        first_number = second_number
+    for i in range(1, n):  #i represents each number of the sequence, while n acts like n-1 since we start at 1 and this way we avoid duplicates
+        print(first_number, end=" ")  #prints the first number
+        third_number = first_number + second_number  #calculates the following number and stores it in the variable third_number
+        first_number = second_number  #updates these two variables for the next iteration
         second_number = third_number
-    return n
+    return second_number  #returns second_number which represents the last term of the sequence
 
-print("5th Fibonacci term: " , fibon(5))
-print("10th Fibonacci term: " , fibon(10))
-print("5th Fibonacci term: " , fibon(15))
+print("\n5th Fibonacci term: " , fibon(5))
+print("\n10th Fibonacci term: " , fibon(10))
+print("\n15th Fibonacci term: " , fibon(15))
