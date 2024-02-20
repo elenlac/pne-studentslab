ce4336a75bba2128660f5bd89a65c4c74e922913
elenlac <e.lacalle.2023@alumnos.urjc.es>
2024-02-20 14:55:10 +0100

    update

--- S06/e2.py
+++ S06/e2.py
@@ -1,23 +1,19 @@
 class Seq:
     def __init__(self, sequence):
-        # Initialize the sequence with the value
-        # passed as argument when creating the object
         self.sequence = sequence
 
     def __str__(self):
         return self.sequence
 
     def len(self):
-        """Calculate the length of the sequence"""
         return len(self.sequence)
 
 
-def print_seqs(seq_list):
-    for seq in seq_list:
-        index = seq_list.index(seq)
-        length = seq.len()  
-        sequence = seq
-        print(f"Sequence {index}: (Length: {length}) {sequence}")
+def print_seqs(seq_list):  # seq_list represents any list containing sequences that belong to the Seq class
+    for seq in seq_list:  # seq represents each of the individual sequences of seq_list
+        index = seq_list.index(seq)  # we use this variable to determine where is our sequence located in the list
+        length = seq.len()  # now we calculate the length of such sequence with the previous function
+        print(f"Sequence {index}: (Length: {length}) {seq}")
 
 
 seq_list = [Seq("ACT"), Seq("GATA"), Seq("CAGATA")]
--- S06/e3.py
+++ S06/e3.py
@@ -1,29 +1,28 @@
 class Seq:
     def __init__(self, sequence):
-        # Initialize the sequence with the value
-        # passed as argument when creating the object
         self.sequence = sequence
 
     def __str__(self):
         return self.sequence
 
     def len(self):
-        """Calculate the length of the sequence"""
         return len(self.sequence)
+
+
+def generate_seqs(pattern, number):  # will create a list with the provided number of sequences(pattern)
+    new_sequences = []  # here we store the new sequences created each iteration
+    for i in range(1, number + 1):  # we start in 1 since we would be returning an empty list in 0
+        sequence = pattern * i  # the current sequence(pattern) will be repeated a determined number of times (i)
+        new_sequences.append(sequence)  # we append the sequence to our list
+        print("New sequence created!")
+    return new_sequences
 
 
 def print_seqs(seq_list):
     for seq in seq_list:
         index = seq_list.index(seq)
         length = seq.len()
-        sequence = seq
-        print(f"Sequence {index}: (Length: {length}) {sequence}")
-
-
-def generate_seqs(pattern, number):  # will create a list with the provided number of sequences
-    result = pattern * number
-
-    return result
+        print(f"Sequence {index}: (Length: {length}) {seq}")
 
 
 seq_list1 = generate_seqs("A", 3)
