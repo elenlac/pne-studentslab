1d2da96549d8a2752dd7bd93aa6fc5847c1a9370
elenlac <e.lacalle.2023@alumnos.urjc.es>
2024-03-06 20:36:11 +0100

    update

--- S10/client_test.py
+++ S10/client_test.py
@@ -10,9 +10,10 @@ c = Client(IP, PORT)
 
 # -- Send a message to the server
 for i in range(5):
-    msg = colored(f"Message {i}", "blue")
-    print(f"To Server: {msg}")
+    message = f"Message {i}"
+    msg_blue = colored(f"Message {i}", "blue")
+    print(f"To Server: {msg_blue}")
+    response = c.talk(message)
 
-    msg2 = colored(f"Message {i}", "green")
-    response = c.talk(msg)
-    print(colored(f"From Server: ECHO: {msg2}\n", "green"))
+    msg_green = colored(f"ECHO: {message}", "green")
+    print(f"From Server: {msg_green}\n")
