349faa7819775a297716acbeb4feb3f46976e025
elenlac <e.lacalle.2023@alumnos.urjc.es>
2024-03-13 11:53:06 +0100

    update

--- notes/Game.py
+++ notes/Game.py
@@ -1,64 +1,75 @@
 import socket
 import random
 
-PORT = 8081
-IP = "127.0.0.1"
-MAX_OPEN_REQUESTS = 5
-number_con = 0
-
-
+"""HERE WE HAVE THE CLASS NUMBERGUESSER, OUR GAME:"""
 class NumberGuesser:
+    MIN = 1
+    MAX = 100
+
     def __init__(self):
-        self.secret_number = random.randint(1, 100)
-        self.attempts = []
+        self.secret_number = random.randint(NumberGuesser.MIN, NumberGuesser.MAX)  # generates a random number
+        self.attempts = []  # here the number of attempts made by the client will be stored
+        self.guessed = False  # the initial value of attribute guessed will be false until it is actually guessed(=True)
 
     def __str__(self):
-        pass
+        return f"Secret number: {self.secret_number} - Attempts: {self.attempts}"
 
-    def guess(self, number):
+    def guess(self, number):  # receives the number of the client, adds it to attempts list, and responds
+        self.attempts.append(number)
         if number < self.secret_number:
-            self.attempts.append(number)
             string = "Higher"
         elif number > self.secret_number:
-            self.attempts.append(number)
             string = "Lower"
         else:
+            self.guessed = True
             x = len(self.attempts)
             string = f"You won after {x} attempts"
-
         return string
 
+    def is_guessed(self):
+        return self.guessed
+
+
+"""HERE WE HAVE THE SERVER CODE:"""
+
+IP = "127.0.0.1"
+PORT = 8080
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 try:
     serversocket.bind((IP, PORT))
-    serversocket.listen(MAX_OPEN_REQUESTS)
+    serversocket.listen()
+    print("'Guess the number' server configured!")
 
     while True:
         print(f"Waiting for connections at {IP}, {PORT}")
         (clientsocket, address) = serversocket.accept()
 
-        number_con += 1
-        print(f"CONNECTION: {number_con}. From the address: {address}")  # address= ip_client + port_client
-        game = NumberGuesser()
+        number = NumberGuesser()  # server creates a new object of this class everytime a client connects
 
-        flag = True
-        while flag:
-            # Receive the message
-            msg = clientsocket.recv(2048).decode("utf-8")
-            print(f"Message from client: {msg}")
+        guessed = False  # establishes guessed as false
+        while not guessed:  # enters the loop
 
-            # Send the message
-            msg = int(msg)
-            guess = game.guess(msg)
-            clientsocket.send(str.encode(guess))
-            flag = False
+            # Receive the message from client
+            request_bytes = clientsocket.recv(2048)
+            request = request_bytes.decode("utf-8")
+            int_request = int(request)
+            print(f"Guess from client: {int_request}")
 
+            # Send the response to the client
+            response = number.guess(int_request)  # response is the string returned by the guess method
+            response_bytes = clientsocket.send(response.encode())
+
+            print(number)  # the object is printed
+
+            if number.is_guessed():  # if it has been guessed, then the boolean turns True to exit the loop
+                guessed = True
         clientsocket.close()
 
 except socket.error:
     print(f"Problems using ip {IP} port {PORT}. Is the IP correct? Do you have port permission?")
-
 except KeyboardInterrupt:
     print("Server stopped by the user")
     serversocket.close()
--- notes/Player.py
+++ notes/Player.py
@@ -1,37 +1,28 @@
-class Client:
-    def __init__(self, server_ip: str, server_port: int):
-        self.server_ip = server_ip
-        self.server_port = server_port
+import socket
+import termcolor
 
-    def __str__(self):
-        return f"Connection to SERVER at {self.server_ip}, PORT: {self.server_port}"
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 8080
 
-    def talk(self, msg):
-        import socket
 
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((SERVER_IP, SERVER_PORT))
 
-        client_socket.connect((self.server_ip, self.server_port))
-
-        msg_bytes = str.encode(msg)
-        client_socket.send(msg_bytes)
+guessed = False
+while not guessed:  # this loop continues unit the client guesses the number(int)
+    try:
+        n = int(input("Enter a number: "))
+        request = str(n)
+        request_bytes = request.encode()
+        client_socket.send(request_bytes)
 
         response_bytes = client_socket.recv(2048)
-        response = response_bytes.decode("utf-8")
-
-        client_socket.close()
-
-        return response
-
-
-IP = "127.0.0.1"
-PORT = 8081
-
-# -- Create a client object
-c = Client(IP, PORT)
-print("Sending a guess to the server...")
-print("\n")
-
-msg = input("Enter a number:")
-response = c.talk(msg)
-print(f"Response: {response}")
+        response = response_bytes.decode()
+        if response == "Lower" or response == "Higher":
+            termcolor.cprint(response, "yellow")
+        else:
+            termcolor.cprint(response, "green")
+            guessed = True
+    except ValueError:
+        print("Please enter a number")
+client_socket.close()
