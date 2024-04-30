f27e479e60f9ab915fe64c48deadf7ae20be6e72
elenlac <e.lacalle.2023@alumnos.urjc.es>
2024-04-30 13:01:02 +0200

    update

---
+++ Final-project/server.py
@@ -0,0 +1,154 @@
+import http.server
+from http import HTTPStatus
+import socketserver
+import termcolor
+from pathlib import Path
+from urllib.parse import urlparse, parse_qs
+import jinja2
+import os
+import json
+
+
+PORT = 8080
+HTML_FOLDER = "html"  # optional, since we could have the html files in the same directory as the server
+EMSEMBL_SERVER = "rest.ensembl.org"  # the IP of the server
+RESOURCE_TO_ENSEMBL_REQUEST = {
+    '/listSpecies': {'resource': "/info/species", 'params': "content-type=application/json"},
+    '/karyotype': {'resource': "/info/assembly", 'params': "content-type=application/json"},
+    '/contentLength': {'resource': "/info/assembly", 'params': "content-type=application/json"}
+}  # dict that contains a resource/endpoint as key with a dict as value. we state what we will request to ensembl
+RESOURCE_NOT_AVAILABLE_ERROR = "Resource not available"
+ENSEMBL_COMMUNICATION_ERROR = "Error in communication with the Ensembl server"
+
+
+def read_html_template(file_name):  # RETURNS A TEMPLATE, we don't use it with index.html (static), or could render()
+    file_path = os.path.join(HTML_FOLDER, file_name)
+    contents = Path(file_path).read_text()
+    contents = jinja2.Template(contents)
+    return contents
+
+
+def server_request(server, url):  # the server we connect to and the url of the request
+    import http.client  # local import, since we are only using it in this function
+
+    error = False
+    data = None
+    try:  # if everything is ok, data = content and error = false
+        connection = http.client.HTTPSConnection(server)  # HTTPS since "ensembl.org" works with a security layer
+        connection.request("GET", url)
+        response = connection.getresponse()  # response is type http response
+        if response.status == HTTPStatus.OK:
+            json_str = response.read().decode()
+            data = json.loads(json_str)
+        else:  # if response is not OK
+            error = True
+    except Exception:  # if there is ANY error in the communication with the server, data = none and error = true
+        error = True
+    return error, data  # returns a duple
+
+
+def handle_error(endpoint, message):  # endpoint: resource received from the user: message: str response
+    # the error.html file has been turned into a TEMPLATE, since the server fills the info depending on the error type
+    context = {
+        'endpoint': endpoint,
+        'message': message
+    }
+    return read_html_template("error.html").render(context=context)
+
+
+def list_species(endpoint, parameters):
+    request = RESOURCE_TO_ENSEMBL_REQUEST[endpoint]  # storing the dict what contains the keys "resource", "params"
+    url = f"{request['resource']}?{request['params']}"
+    error, data = server_request(EMSEMBL_SERVER, url)  # we use our function
+    if not error:
+        limit = None  # REMEMBER LIMIT IS OPTIONAL("None" as default), so if we receive it:
+        if 'limit' in parameters:
+            limit = int(parameters['limit'][0])  #
+        """print(data)"""
+
+        """WE PARSE THE INFO FROM ENSEMBL"""
+        species = data['species']  # list<dict>, each species is a dict
+        name_species = []
+        for specie in species[:limit]:
+            name_species.append(specie['display_name'])
+        context = {
+            'number_of_species': len(species),
+            'limit': limit,
+            'name_species': name_species  # list
+        }
+        contents = read_html_template("species.html").render(context=context)
+        code = HTTPStatus.OK
+    else:
+        contents = handle_error(endpoint, ENSEMBL_COMMUNICATION_ERROR)  # we use our function and state a general error
+        code = HTTPStatus.SERVICE_UNAVAILABLE  # we change code to "503"
+    return code, contents  # returns a duple
+
+def karyotype(endpoint, parameters):
+    request = RESOURCE_TO_ENSEMBL_REQUEST[endpoint]
+    specie = parameters['species'][0]
+    url = f"{request['resource']}/{specie}?{request['params']}"
+    error, data = server_request(EMSEMBL_SERVER, url)  # we use our function
+    if not error:
+        """print(data)"""
+
+        """WE PARSE THE INFO FROM ENSEMBL"""
+        context = {
+            'specie': specie,
+            'karyotype': data['karyotype']
+        }
+        contents = read_html_template("karyotype.html").render(context=context)
+        code = HTTPStatus.OK
+    else:
+        contents = handle_error(endpoint, ENSEMBL_COMMUNICATION_ERROR)
+        code = HTTPStatus.SERVICE_UNAVAILABLE
+    return code, contents
+
+
+"""MAIN PROGRAM"""
+
+socketserver.TCPServer.allow_reuse_address = True
+
+
+class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        termcolor.cprint(self.requestline, 'green')
+
+        parsed_url = urlparse(self.path)
+        endpoint = parsed_url.path  # also known as resource/ path
+        print(f"Endpoint: {endpoint}")
+        parameters = parse_qs(parsed_url.query)  # takes the request and gets the info from "?": it is a dictionary
+        print(f"Parameters: {parameters}")
+
+        code = HTTPStatus.OK  # we establish "200" as default
+        content_type = "text/html"
+        contents = ""  # empty str, not filled yet
+        if endpoint == "/":  # THIS FILE IS NOT A TEMPLATE, it is a STATIC WEB PAGE since server doesn't fill anything
+            file_path = os.path.join(HTML_FOLDER, "index.html")
+            contents = Path(file_path).read_text()
+        elif endpoint == "/listSpecies":
+            code, contents = list_species(endpoint, parameters)  # we use our function
+        elif endpoint == "/karyotype":
+            code, contents = karyotype(endpoint, parameters)  # we use our function
+        elif endpoint == "/chromosomeLength":
+            code, contents = chromosomeLength(endpoint, parameters)
+        else:
+            contents = handle_error(endpoint, RESOURCE_NOT_AVAILABLE_ERROR)  # we use our function
+            code = HTTPStatus.NOT_FOUND  # we change code to "404"
+
+        self.send_response(code)
+        contents_bytes = contents.encode()
+        self.send_header('Content-Type', content_type)
+        self.send_header('Content-Length', str(len(contents_bytes)))
+        self.end_headers()
+
+        self.wfile.write(contents_bytes)
+
+
+with socketserver.TCPServer(("", PORT), MyHTTPRequestHandler) as httpd:
+    print("Serving at PORT...", PORT)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print()
+        print("Stopped by the user")
+        httpd.server_close()
