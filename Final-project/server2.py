165b1dfda3a41c3b97503d11cc9cdd13d1fb45f8
elenlac <e.lacalle.2023@alumnos.urjc.es>
2024-05-26 18:18:19 +0200

    update

--- Final-project/server2.py
+++ Final-project/server2.py
@@ -25,6 +25,18 @@ RESOURCE_TO_ENSEMBL_REQUEST = {
 RESOURCE_NOT_AVAILABLE_ERROR = "Resource not available"
 ENSEMBL_COMMUNICATION_ERROR = "Error in communication with the Ensembl server"
 GENES = ["ADA", "FRAT1", "FXN", "RNU6_269P", "U5"]  # for reference
+IDs = {
+    "FRAT1": "ENSG00000165879",
+    "ADA": "ENSG00000196839",
+    "FXN": "ENSG00000165060",
+    "RNU6_269P": "ENSG00000212379",
+    "MIR633": "ENSG00000207552",
+    "TTTY4C": "ENSG00000228296",
+    "RBMY2YP": "ENSG00000227633",
+    "FGFR3": "ENSG00000068078",
+    "KDR": "ENSG00000128052",
+    "ANK2": "ENSG00000145362"
+}
 
 
 def read_html_template(file_name):  # RETURNS A TEMPLATE, we don't use it with index.html (static)
